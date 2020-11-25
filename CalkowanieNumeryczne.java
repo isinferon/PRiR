78ac2639ad4219efa500ffcab15f37525b4ad6ed
isinferon <62243130+isinferon@users.noreply.github.com>
2020-11-25 12:41:22 +0100

    Delete CalkowanieNumeryczne.java

--- CalkowanieNumeryczne.java
+++
@@ -1,9 +0,0 @@
-
-import java.util.function.Function;
-
-interface CalkowanieNumeryczne {
-    public static double oblicz(double poczatek, double koniec, int n, Function<Double, Double> f)
-    {
-        return -1;
-    };
-}
